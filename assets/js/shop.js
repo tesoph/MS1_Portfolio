06353117e028cf4d75856706f4a33c81d34a34b6
ray <12276029+tesoph@users.noreply.github.com>
2019-10-08 20:56:52 +0100

    Add assets/js and update scripts section

---
+++ assets/js/shop.js
@@ -0,0 +1,8 @@
+  var white = document.querySelector("#white");
+        white.onclick = function () {
+            document.getElementById('myImage').src = 'assets/img/shop/tote-white.jpg';
+        }
+        var black = document.querySelector("#black");
+        black.onclick = function () {
+            document.getElementById('myImage').src = 'assets/img/shop/tote-black.jpg';
+        }
